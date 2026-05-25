0d8a70e454261c1dcda507055dbf9eb73d354efb
feafania <173483105+feafania@users.noreply.github.com>
2026-05-25 17:27:19 +0100

    feat: add download button component

--- src/components/result-flyout/SelectedFlyout.tsx
+++ src/components/result-flyout/SelectedFlyout.tsx
@@ -2,9 +2,7 @@ import { useSelectedItemsStore } from '../../store';
 import type { Character } from '../../types/types.ts';
 
 import './selected-flyout.css';
-import { buildCSV } from '../../util/export/buildCsv.ts';
-import { downloadFile } from '../../util/export/downloadFile.ts';
-import { fetchCharactersByIds } from '../../util/fetchCharactersByIds.ts';
+import { DownloadButton } from '../DownloadButton.tsx';
 
 type SelectedFlyoutProps = {
   results: Character[];
@@ -16,30 +14,6 @@ export function SelectedFlyout({ results }: SelectedFlyoutProps) {
   const toggleItems = useSelectedItemsStore((state) => state.toggleItems);
 
   if (selectedItems.length === 0) return null;
-
-  const handleDownload = async () => {
-    const ids = selectedItems;
-
-    const characters = await fetchCharactersByIds(ids);
-
-    const csv = buildCSV(
-      characters,
-
-      ['Name', 'Gender', 'Birth year', 'URL'],
-
-      (item) => {
-        if (item.status === 'fulfilled') {
-          const c = item.data;
-
-          return [c.name, c.gender, c.birth_year, c.url];
-        }
-
-        return [`UNKNOWN (${item.id})`, 'N/A', 'N/A', `N/A`];
-      }
-    );
-
-    downloadFile(csv, `${ids.length}_items.csv`);
-  };
 
   const ids = results
     .map((character) => character.url.match(/people\/(\d+)\//)?.[1])
@@ -59,24 +33,7 @@ export function SelectedFlyout({ results }: SelectedFlyoutProps) {
         <button className="toggle-button" onClick={() => toggleItems(ids)}>
           Toggle page
         </button>
-        <button className="download-button" onClick={handleDownload}>
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-            <path d="M7 10l5 5 5-5" />
-            <path d="M12 15V3" />
-          </svg>
-
-          <span>Download</span>
-        </button>
+        <DownloadButton selectedItems={selectedItems} />
       </div>
     </div>
   );
